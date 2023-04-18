04553593c0abd2c99f52492b662e50022c841238
asdrubal22 <henry_rodriguez23201@elpoli.edu.co>
2023-04-17 21:24:09 -0500

    first commit

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,7 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
